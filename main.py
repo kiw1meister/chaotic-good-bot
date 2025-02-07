d8e648b9b69581a21d494edc910b23ef7cacfe6b
Andy Lin <linandy719@gmail.com>
2025-02-07 17:47:33 +0800

    feat: test response code

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ load_dotenv()
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}.')
+    
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
