0c3df6d2348a557436cd6bd9de238d3aaa89c236
Andy Lin <linandy719@gmail.com>
2025-02-11 17:59:49 +0800

    feat: test reaction code

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Client(discord.Client):
             print(f'Message from {message.author}: {message.content}')
             await message.channel.send(f'Yo {message.author.name}')
 
+    async def on_reaction_add(self, reaction, user):
+        await reaction.message.channel.send('Reaction confirmed')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
